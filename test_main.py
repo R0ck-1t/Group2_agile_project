7046cec0f4980f5dc35db29449b122cfb30c5968
informationvulture <schoolemailnm1@gmail.com>
2022-05-12 12:16:07 -0700

    Add more Flask unit tests to meet W3 requirements

--- test_main.py
+++ test_main.py
@@ -4,12 +4,27 @@ from main import app
 class MyTestCase(unittest.TestCase):
 
     def test_root(self):
+        """
+        Test that the root page is returning the correct status code.
+        """
         self.app = app.test_client()
         r = self.app.get('/')
         assert r.status_code == 200
 
 
     def test_random(self):
+        """
+        Test that random pages return a 404, indicates the the webpage
+        is working with regard to routes that are incorrect.
+        """
         self.app = app.test_client()
         r = self.app.get('/nonexistant')
         assert r.status_code == 404
+
+    def test_replit(self):
+        """
+        Test that the replit page is rendering the correct template.
+        """
+        self.app = app.test_client()
+        r = self.app.get('/replit')
+        assert 'Demo Game' in str(r.data)
