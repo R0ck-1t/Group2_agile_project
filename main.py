20039f96cf0be493afe9a8205c57c93b13c437bb
petervanderhook <petervanderhook@gmail.com>
2022-05-17 15:54:32 -0700

    Added comments posting, getting, and deleting. Changed routing for viewing submission

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from src.loginform import LoginForm
 from src.regform import RegisterForm
 from src.userform import UserForm
+from src.commentform import commentForm
 from src.submissionform import submissionForm
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 import os
@@ -45,6 +46,13 @@ class Replit(db.Model):
   replit_uploader_id = db.Column(db.Integer, db.ForeignKey('user.id'))
   replit_description = db.Column(db.String(512), default = "")
 
+class Comment(db.Model):
+  __tablename__ = 'comment'
+  comment_id = db.Column(db.Integer, primary_key = True)
+  replit_id = db.Column(db.Integer, db.ForeignKey('replit.replit_id'))
+  content = db.Column(db.String(512))
+  user = db.Column(db.String(24), db.ForeignKey('user.username'))
+
 db.create_all()
 db.session.commit()
 def ensure_test_account():
@@ -64,23 +72,59 @@ def home():
 
 @app.route('/home', methods=['GET', 'POST'])
 def index():
-  if current_user.is_authenticated:
-    return render_template('replit_page.html', link="https://replit.com/@informationvult/2911Demo/?outputonly=1&embed=1", name=current_user.username, email = current_user.email, bio=current_user.bio)
-  else:
-    return render_template('replit_page.html', link="https://replit.com/@informationvult/2911Demo/?outputonly=1&embed=1")
+  form=commentForm()
+  return redirect(url_for('view_content', form=form, replit_id_num=3))
   
-@app.route('/view/<int:replit_id_num>', methods=['GET'])
+@app.route('/view/<int:replit_id_num>', methods=['GET', 'POST'])
 def view_content(replit_id_num):
+  form = commentForm()
   replit_submission = Replit.query.filter_by(replit_id = replit_id_num).first()
-  return render_template('replit_page.html', link = replit_submission.replit_link)
+  all_comments = Comment.query.all()
+  post_comments = []
+  for row in all_comments:
+    print(f"Comment: {row.content}\nReplit ID: {row.replit_id} - Current Replit ID: {replit_id_num}")
+    if row.replit_id == replit_id_num:
+      post_comments.append(row)
+  if form.validate_on_submit():
+    new_comment = Comment(content=form.content.data, user=current_user.username, replit_id = replit_id_num)
+    for item in post_comments:
+      if item.content == new_comment.content:
+        if item.user == current_user.username:
+          return redirect(url_for('view_content', replit_id_num=replit_id_num))
+    db.session.add(new_comment)
+    print(new_comment)
+    db.session.commit()
+    form.content.data = ""
+  all_comments = Comment.query.all()
+  post_comments = []
+  for row in all_comments:
+    print(f"Comment: {row.content}\nReplit ID: {row.replit_id} - Current Replit ID: {replit_id_num}")
+    if row.replit_id == replit_id_num:
+      post_comments.append(row)
+  return render_template('replit_page.html', form=form, comments = post_comments, replit_name=replit_submission.replit_name, description=replit_submission.replit_description, link = replit_submission.replit_link)
 
+
+@app.route('/submit_comment')
+@login_required
+def submit_comment():
+  pass
+
+@app.route('/delete_comment/<comment_id_num>', methods=['GET', 'DELETE'])
+@login_required
+def delete_comment(comment_id_num):
+  comment = Comment.query.filter_by(comment_id = comment_id_num).first()
+  replit_id = comment.replit_id
+  Comment.query.filter_by(comment_id = comment_id_num).delete()
+  db.session.commit()
+  return redirect(url_for('view_content', replit_id_num = replit_id))
 
 @app.route('/submissions', methods=['GET'])
 def submissions():
+  replits = Replit.query.all()
   if current_user.is_authenticated:
-    return render_template('user_submissions.html', name=current_user.username, email = current_user.email, bio=current_user.bio)
+    return render_template('user_submissions.html', name=current_user.username, email = current_user.email, bio=current_user.bio, replits=replits)
   else:
-    return render_template('user_submissions.html')
+    return render_template('user_submissions.html', replits=replits)
 
 @app.route('/new_submission', methods=['GET', 'POST'])
 @login_required
@@ -91,9 +135,11 @@ def new_submission():
       flash('Error. Invalid URL.', 'error')
       return
     else:
-      url = form.replit_link.data.split('?')
-      url[0] = url[0] + '?embed=true'
-    new_replit = Replit(replit_link=url[0], replit_name=form.replit_name.data, replit_description=form.replit_description.data, replit_uploader_id=current_user.id)
+      url = form.replit_link.data.split('#')
+      url = url[0]
+      url = url.split('?')
+      url = url[0] + '?embed=true'
+    new_replit = Replit(replit_link=url, replit_name=form.replit_name.data, replit_description=form.replit_description.data, replit_uploader_id=current_user.id)
     db.session.add(new_replit)
     db.session.commit()
     return redirect(url_for('submissions'))
